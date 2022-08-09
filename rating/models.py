56cd7b362dc5432eeea2034ac1541cffd9f9ad3b
elamirkad <elamirkad@gmail.com>
2022-08-09 17:40:11 +0600

    Professor and course models created

--- rating/models.py
+++ rating/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Professor(models.Model):
+    name = models.CharField(max_length=255)
+    overall = models.FloatField()
+    easy = models.FloatField()
+    knowledge = models.FloatField()
+    fun = models.FloatField()
+
+
+class Course(models.Model):
+    name = models.CharField(max_length=255)
+    professors = models.ManyToManyField(Professor)
