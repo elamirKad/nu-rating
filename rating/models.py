d4bd38bb1ae5a1419ab31a3df5eeb36031ae7dad
elamirkad <elamirkad@gmail.com>
2022-08-09 18:17:32 +0600

    First page setup

--- rating/models.py
+++ rating/models.py
@@ -3,10 +3,10 @@ from django.db import models
 # Create your models here.
 class Professor(models.Model):
     name = models.CharField(max_length=255)
-    overall = models.FloatField()
-    easy = models.FloatField()
-    knowledge = models.FloatField()
-    fun = models.FloatField()
+    overall = models.FloatField(default=0.0)
+    easy = models.FloatField(default=0.0)
+    knowledge = models.FloatField(default=0.0)
+    fun = models.FloatField(default=0.0)
 
 
 class Course(models.Model):
--- rating/views.py
+++ rating/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Professor, Course
+
 
 # Create your views here.
+def main(request):
+    courses = Course.objects.all()
+    dic = {
+        'courses': courses
+    }
+    return render(request, 'index.html', dic)
