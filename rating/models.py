7fa77f2dd25591631d4e258bb43909b9808cab3f
elamirkad <elamirkad@gmail.com>
2022-08-10 10:52:25 +0600

    Comments system UI

--- rating/models.py
+++ rating/models.py
@@ -3,12 +3,16 @@ from django.db import models
 # Create your models here.
 class Professor(models.Model):
     name = models.CharField(max_length=255)
-    overall = models.FloatField(default=0.0)
-    easy = models.FloatField(default=0.0)
-    knowledge = models.FloatField(default=0.0)
-    fun = models.FloatField(default=0.0)
+
 
 
 class Course(models.Model):
     name = models.CharField(max_length=255)
     professors = models.ManyToManyField(Professor)
+
+class Comment(models.Model):
+    text = models.TextField()
+    overall = models.FloatField(default=0.0)
+    easy = models.FloatField(default=0.0)
+    knowledge = models.FloatField(default=0.0)
+    fun = models.FloatField(default=0.0)
