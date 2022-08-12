400d2b6872de9a9fdc3bd2fc724d22d4000347c1
elamirkad <elamirkad@gmail.com>
2022-08-12 16:33:50 +0600

    Heroku settings

--- nurating/settings.py
+++ nurating/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'XPhQBM89$ChP8eY#@@OJB924ghWDe*dYgh5!NPa*D&EkE&RGJ4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
